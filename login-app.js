f13e197cf15d26c361f5932b0b47f7bb22c68659
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-27 16:26:02 -0500

    Maybe this one...

--- login-app.js
+++ login-app.js
@@ -2,19 +2,60 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const mustache = require('mustache-express')
+const expressSession = require('express-session');
 
-app.listen(3000,function(){
+
+app.listen(3000, function() {
   console.log("here I go! zooom!~")
 })
 
-app.engine('mustache', mustache() )
+app.engine('mustache', mustache())
 app.set('view engine', 'mustache');
 
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 
-app.get('/', function(req,res){
-    res.render("home",{
-      pageTitle: "Home!",
-    })
+app.use(expressSession({
+  secret: 'hubbawubba',
+  resave: false,
+  saveUninitialized: true
+}))
+
+var userArray = [{
+  username: "bob1",
+  password: "1234"
+}, {
+  username: "jill1",
+  password: "pass"
+}];
+
+var loginAttempt = false;
+
+app.get('/', function(req, res) {
+  res.render("home", {
+    pageTitle: "Home!",
+  })
+})
+
+app.get('/login/', function(req, res) {
+  res.render("login", {
+    pageTitle: "Login!",
+  })
+})
+
+app.post('/logMeIn', function(req, res) {
+  let nameEntry = req.body.username;
+  let passEntry = req.body.password;
+  console.log("username: " + nameEntry + " " + "password: " + passEntry);
+  for (i = 0; i < userArray.length; i++) {
+    if (nameEntry === userArray[i].username) {
+      console.log("USERNAME MATCH")
+      if (passEntry === userArray[i].password) {
+        console.log("PASSWORD MATCH")
+      }
+    }
+  }
+  res.redirect('/')
 })
