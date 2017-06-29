3719e2514f2ffb1e0bf0b0723ace1baa4f071a2d
Taylor Gaines <taylorcgaines@gmail.com>
2017-06-28 21:51:11 -0500

    I feel like I am so close on this, something about headers happening twice, but don't know how to fix it

--- login-app.js
+++ login-app.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 const mustache = require('mustache-express')
 const expressSession = require('express-session');
 
-
 app.listen(3000, function() {
   console.log("here I go! zooom!~")
 })
@@ -23,23 +22,25 @@ app.use(expressSession({
   saveUninitialized: true
 }))
 
+var loggedIn= false
+
 var userArray = [{
   username: "bob1",
-  password: "1234"
+  password: "1234",
 }, {
   username: "jill1",
-  password: "pass"
+  password: "pass",
 }];
 
-var loginAttempt = false;
-
 app.get('/', function(req, res) {
-  res.render("home", {
-    pageTitle: "Home!",
-  })
+  if (loggedIn === false) {
+      res.redirect("/login")
+    } else{
+  res.render("home")
+  }
 })
 
-app.get('/login/', function(req, res) {
+app.get('/login', function(req, res) {
   res.render("login", {
     pageTitle: "Login!",
   })
@@ -54,6 +55,10 @@ app.post('/logMeIn', function(req, res) {
       console.log("USERNAME MATCH")
       if (passEntry === userArray[i].password) {
         console.log("PASSWORD MATCH")
+        loggedIn= true
+        res.render('home', {
+          nameEntry: userArray[i].username
+        })
       }
     }
   }
